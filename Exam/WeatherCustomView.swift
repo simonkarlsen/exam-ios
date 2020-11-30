f66ea90039758f5fcfefbbf6a2de0c83cfdfe6ea
simonkarlsen <si-karlsen@hotmail.com>
2020-11-30 03:18:29 +0100

    fixed missing file

--- Exam/WeatherCustomView.swift
+++ Exam/WeatherCustomView.swift
@@ -2,38 +2,59 @@
 //  WeatherCustomView.swift
 //  Exam
 //
-//  
+//
 //
 
 import UIKit
 
-class WeatherCustomView: UIView {
 
+protocol CustomDelegate {
+    func giveDataToCustomView(latitudeLabel: String, longitudeLabel: String, imageName: String)
+}
+
+
+class WeatherCustomView: UIView {
     
-    @IBOutlet var customView: UIView!
     
-    
+    //    @IBOutlet var customView: UIView!
+    //
     @IBOutlet weak var latLabel: UILabel!
     
+    @IBOutlet weak var lonLabel: UILabel!
     
-    required init?(coder aDecoder: NSCoder) {
-        super.init(coder: aDecoder)
+    @IBOutlet weak var imageView: UIImageView!
+    
+    var customDelegate: CustomDelegate!
+    
+    override func awakeFromNib() {
+//                imageView.layer.cornerRadius = 15
+//                lonLabel.layer.cornerRadius = 15
+//                latLabel.layer.cornerRadius = 15
+//
+//                imageView.layer.masksToBounds = true
+//                lonLabel.layer.masksToBounds = true
+//                latLabel.layer.masksToBounds = true
+    }
+    
+    func giveInfo (latitudeInfo: String, longitudeInfo: String, imageInfo: String) {
         
-        let _ = loadViewFromNib()
-    }
-    
-    func loadViewFromNib() -> UIView {
-        let bundle = Bundle.init(for: type(of: self))
-        let nib = UINib(nibName: "WeatherCustomView", bundle: bundle)
-        let view = nib.instantiate(withOwner: self, options: nil)[0] as! UIView
+        customDelegate.self.giveDataToCustomView(latitudeLabel: latitudeInfo, longitudeLabel: longitudeInfo, imageName: imageInfo)
         
-        addSubview(view)
-        return view
     }
     
-    func setLabel(_ string: String) {
-        latLabel.text = string
+    func xibInit(imageName: String, lonInfo: String, latInfo: String) {
+        imageView.image = UIImage(named: imageName)
+        lonLabel.text = lonInfo
+        latLabel.text = latInfo
+
+        
+        print("imageName: \(imageName), lonInfo: \(lonInfo), latInfo: \(latInfo)")
     }
+    
+    
 }
+
+
+
 
 
